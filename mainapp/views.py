b2f142f6101c1e8493e1d947918492cb958a40af
banderasvitali <banderolvitali@gmail.com>
2022-11-08 00:42:30 +0300

    made "email_to_link" filter

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,34 @@
 from django.views.generic import TemplateView
+from datetime import datetime
 
 
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
+    
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['contacts'] = [
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHcrhA',
+                'city': 'Казань',
+                'phone': '+7-999-22-22222',
+                'email': 'geeklab@spb.ru',
+                'address': 'Территория Петропавловская крепость, 3Ж',
+            },{
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHX3xB',
+                'city': 'Санкт‑Петербург',
+                'phone': '+7-999-11-11111',
+                'email': 'geeklab@kz.ru',
+                'address': 'Территория Кремль, 11, Казань, Республика Татарстан, Россия',
+            },{
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHh9kD',
+                'city': 'Москва',
+                'phone': '+7-999-33-33333',
+                'email': 'geeklab@msk.ru',
+                'address': 'Красная площадь, 7, Москва, Россия',
+            },
+        ]
+        return context_data
 
 
 class CoursesListView(TemplateView):
@@ -30,23 +56,23 @@ class NewsView(TemplateView):
             {
                 'title': 'Новость 1',
                 'preview': 'Предварительное описание новости',
-                'date': '2022-11-07'
+                'date': datetime.now()
             },{
                 'title': 'Новость 2',
                 'preview': 'Предварительное описание новости',
-                'date': '2022-11-07'
+                'date': datetime.now()
             },{
                 'title': 'Новость 3',
                 'preview': 'Предварительное описание новости',
-                'date': '2022-11-07'
+                'date': datetime.now()
             },{
                 'title': 'Новость 4',
                 'preview': 'Предварительное описание новости',
-                'date': '2022-11-07'
+                'date': datetime.now()
             },{
                 'title': 'Новость 5',
                 'preview': 'Предварительное описание новости',
-                'date': '2022-11-07'
+                'date': datetime.now()
             },
         ]
         return context_data
